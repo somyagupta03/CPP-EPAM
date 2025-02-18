6b97b9e5c10b21b80cc8ea3cc213e0d8d6d294cb
Somya gupta <103734659+somyagupta03@users.noreply.github.com>
2025-02-18 19:20:49 +0530

    Update struct.c

--- STRUCTURE/struct.c
+++ STRUCTURE/struct.c
@@ -1,36 +1,13 @@
+// Structure pointers 
 #include<stdio.h>
-#include <stdlib.h>
-#include<string.h>
 
 struct Student {
 	int age;
-	char *name;
+	char name[50];
 };
-
+struct Student s1 = { 21, "somya" };
 int main() {
-	struct Student* p;
-	//Allocating memory for the structure
-	p = (struct Student*)malloc(sizeof(struct Student));
-	if (p == NULL) {
-		printf("Memory allocation for structure failed\n");
-		return 1;  // Exit the program if allocation fails
-	}
-
-	//Allocating memory for the name
-	p->name = (char*)malloc(50 * sizeof(char));
-	if (p->name == NULL) {
-		printf("Memory allocation for name failed\n");
-		free(p);  // Free the allocated structure before exiting
-		return 1;
-	}
-	
-	p->age = 21;
-	strcpy_s((*p).name, 50, "Somya");
-	printf("Name: %s\n", p->name);
-	printf("Age: %d\n", p->age);
-
-	free(p->name);
-	free(p);
-	
+	struct Student* ptr = &s1;
+	printf("Name: %s, Age: %d\n", ptr->name, ptr->age);
 	return 0;
 }
