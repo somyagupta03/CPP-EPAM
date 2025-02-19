cdb01c56642db6e87691957903e1df92f466bec8
Somya gupta <103734659+somyagupta03@users.noreply.github.com>
2025-02-19 18:20:50 +0530

    Update PointerArithmetic.c

--- Pointers in c/PointerArithmetic.c
+++ Pointers in c/PointerArithmetic.c
@@ -2,9 +2,17 @@
 #include<stdlib.h>
 
 int main(){
+    /*
     int a = 65;
     int *ptr = &a;
     printf("%d\n",ptr);
     printf("%d",ptr+1);
-    return 0;
+    */
+   int arr[]={1,2,3,4,5,67};
+   printf("%d\n",arr[0]);//normal way 
+   //using pointers,finding the value at those positions
+
+   printf("the value at address of second element of array is %d\n",*(arr+1));
+   printf("The value at second position is %d\n",*(&arr[1]));
+   return 0;
 }
