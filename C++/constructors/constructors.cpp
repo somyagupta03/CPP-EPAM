b88161eac20af319605f3083aabe4a1429c7a2ab
Somya gupta <103734659+somyagupta03@users.noreply.github.com>
2025-02-25 12:19:34 +0530

    Create constructors.cpp

---
+++ C++/constructors/constructors.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+class Person{
+private:
+string name;
+int age;
+
+public:
+   //creating a parametrized constructor
+    Person(string a,int b){
+    name=a;
+    age=b;
+    }
+
+    void display(){
+        cout<<"Name :"<<name<<endl;
+        cout<<"age: "<<age<<endl;
+    }
+};
+int main(){
+    Person p1("somya",21); 
+    p1.display();
+
+
+}
