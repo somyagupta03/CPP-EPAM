e9b5a1f8aadbc4058ea9c237ab69538b01e418dd
Somya gupta <103734659+somyagupta03@users.noreply.github.com>
2025-03-04 16:29:32 +0530

    Create subtractoverloading.cpp

---
+++ Operator overloading/subtractoverloading.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+class Vector2D {
+public:
+    float x, y;
+
+    Vector2D(float x, float y) : x(x), y(y) {}
+
+    // Binary subtraction operator
+    Vector2D operator-(const Vector2D& other) const {
+        return Vector2D(x - other.x, y - other.y);
+    }
+
+    void print() const {
+        std::cout << "(" << x << ", " << y << ")" << std::endl;
+    }
+};
+
+int main() {
+    Vector2D vec1(5.0, 4.0);
+    Vector2D vec2(2.0, 1.0);
+
+    Vector2D result = vec1 - vec2; // Using overloaded operator-
+    result.print(); // Output should be (3, 3)
+
+    return 0;
+}
