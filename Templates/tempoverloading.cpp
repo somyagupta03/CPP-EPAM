70cc7ef9462d908964488719bfa31ab4ae1d84b4
Somya gupta <103734659+somyagupta03@users.noreply.github.com>
2025-03-11 17:13:20 +0530

    Create tempoverloading.cpp

---
+++ Templates/tempoverloading.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+template<class T>
+class Harry{
+    public:
+    T data;
+    Harry(T a){
+        data = a;
+    }
+    void display();
+};
+template <class T>
+void Harry<T>::display(){
+    cout<<data;
+}
+
+int main(){
+    Harry<float>h(5.7);
+    cout<<h.data<<endl;
+    h.display();
+    return 0;
+}
